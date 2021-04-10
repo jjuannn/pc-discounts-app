ba185ace8dc4f6f3ee4d630e56863583135aabe8
juan <juancraft12yt@gmail.com>
2021-04-09 21:44:19 -0300

    Update error messages

--- src/services/getList.js
+++ src/services/getList.js
@@ -3,7 +3,9 @@ const BASE_URL = "https://www.cheapshark.com/api/1.0/deals?storeID=1&onSale=1&";
 
 export function getPage(page) {
   return fetch(`${BASE_URL}pageNumber=${page}`)
-    .then((r) => (!r.ok ? new Error("API ERROR") : r))
+    .then((r) =>
+      !r.ok ? new Error(`Cannot get page nº${page}. Try again! :(`) : r
+    )
     .then((r) => r.json())
     .then((jsonResponse) => {
       return jsonResponse.map((offer) => {
@@ -16,7 +18,9 @@ export function getByPriceBetween(page, minPrice, higherPrice) {
   return fetch(
     `${BASE_URL}pageNumber=${page}&lowerPrice=${minPrice}&upperPrice=${higherPrice}`
   )
-    .then((r) => (!r.ok ? new Error("API ERROR") : r))
+    .then((r) =>
+      !r.ok ? new Error(`Cannot get results for you filters. Try again! :(`) : r
+    )
     .then((r) => r.json())
     .then((jsonResponse) => {
       return jsonResponse.map((offer) => {
@@ -27,7 +31,9 @@ export function getByPriceBetween(page, minPrice, higherPrice) {
 
 export function getByTitle(title) {
   return fetch(`${BASE_URL}title=${title}`)
-    .then((r) => (!r.ok ? new Error("API ERROR") : r))
+    .then((r) =>
+      !r.ok ? new Error(`Cannot get results for '${title}'. Try again! :(`) : r
+    )
     .then((r) => r.json())
     .then((jsonResponse) => {
       return jsonResponse.map((offer) => {
